71f542d178a9acfa51da98abba7b6888a1737a2a
fbry-alx <fabry.pappacena@gmail.com>
2020-06-17 15:15:41 -0700

    created profile model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User 
 # Create your models here.
+
+from datetime import date
+
+CURRENT_CITY = (
+  ('LDN', 'London'),
+  ('SYD', 'Sydney'),
+  ('SFO', 'San Francisco'),
+  ('SEA', 'Seattle'),
+)
+
+class Profile(models.Model):
+  name = models.CharField(max_length=100)
+  current_city = models.CharField(max_length=3, choices=CURRENT_CITY, default=CURRENT_CITY[0][0])
+  join_date = models.DateField(default=date.today())
+  
+  
+
+
