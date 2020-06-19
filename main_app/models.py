3130391646b9dcd1e51460c06aa0847642bbad13
ishepard <ishepard@ucsd.edu>
2020-06-19 12:18:40 -0700

    Added str dunder method to models, profile page rough draft, added max-length to descripotion

--- main_app/models.py
+++ main_app/models.py
@@ -15,11 +15,17 @@ class Profile(models.Model):
   current_city = models.CharField(max_length=3, choices=CURRENT_CITY, default=CURRENT_CITY[0][0])
   user=models.ForeignKey(User, on_delete=models.CASCADE, default='1')
   
+  def __str__(self):
+    return self.user.username
+  
 class Post(models.Model):
   title = models.CharField(max_length=100)
   current_city = models.CharField(max_length=3, choices=CURRENT_CITY, default=CURRENT_CITY[0][0])
-  description = models.TextField(max_length=250)
+  description = models.TextField(max_length=1000)
   profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return self.title 
 
 class City(models.Model):
   post = models.ForeignKey(Post, on_delete=models.CASCADE)
