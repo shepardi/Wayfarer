74b75fa36a274c696fe723ee1154de782e8e3e7f
ivoka <ivailo96g@gmail.com>
2020-06-23 19:43:15 -0400

    pagination addedd

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -1,4 +1,9 @@
 let orange = $('#city-name');
+let posts= $('.posts');
+let btnPrev=$('#prev')
+let btnNext=$('#next')
+let paginationNumber=0
+
 
 if (orange.val() == 'London') {
   $('#london').addClass("current")
@@ -14,3 +19,39 @@ if (orange.val() == 'Seattle') {
 };
 
 
+function pagination(number){
+   console.log(posts.length)
+if(number==0){
+  btnPrev.css('visibility' , 'hidden')
+}
+else{
+  btnPrev.css('visibility' , 'visible')
+}
+if(number+4>=posts.length){
+  btnNext.css('visibility' , 'hidden')
+}
+else{
+  btnNext.css('visibility' , 'visible')
+}
+let temp=number
+for( let i=0 ; i<posts.length ; i++  ){
+  posts.eq(i).eq(0).css('display' , 'none')
+}
+for( number ; number<temp+4 ; number++  ){
+    posts.eq(number).eq(0).css('display' , 'block')
+}
+
+}
+
+pagination(paginationNumber)
+
+
+btnPrev.on('click',function(){
+  paginationNumber-=4
+  pagination(paginationNumber)
+})
+
+btnNext.on('click',function(){
+  paginationNumber+=4
+  pagination(paginationNumber)
+})
